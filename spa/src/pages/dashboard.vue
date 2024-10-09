da37ed7b9d00e39b86f046d2fe99b60e6ff46439
Yosef Hoffman <yohoffman@gmail.com>
2024-10-09 19:52:06 -0400

    Create wired up dashboard page

---
+++ spa/src/pages/dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Dashboard</h1>
+  <v-container>
+    <v-row>
+      <v-col v-for="framework in frameworks" :key="framework.id">
+        <v-card @click="viewFramework(framework.id)">
+          <v-card-title>{{ framework.short_name }}</v-card-title>
+          <v-card-subtitle>{{ framework.percent_complete }}%</v-card-subtitle>
+          <v-progress-linear :model-value="framework.percent_complete" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      frameworks: [],
+    };
+  },
+  created() {
+    fetch(import.meta.env.VITE_AXIOS_BASE_URL + 'dashboard')
+      .then(response => response.json())
+      .then((data) => {
+        this.frameworks = data;
+      });
+  },
+  methods: {
+    viewFramework(id) {
+      this.$router.push(`/framework/${id}`);
+    },
+  },
+};
+</script>
