5947d20a394d79666f5eb007216d3b37cc7f2e3c
Yosef Hoffman <yohoffman@gmail.com>
2024-10-15 23:50:12 -0400

    Loader on framework details page

--- spa/src/pages/frameworks/[id].vue
+++ spa/src/pages/frameworks/[id].vue
@@ -1,11 +1,20 @@
 <template>
-  <h1>{{framework.short_name}}</h1>
-  <h2>{{framework.full_name}}</h2>
-  <h3>{{framework.source}} - {{framework.geography}}</h3>
-  <a :href="framework.url">Source</a>
-  <v-card class="ma-4">
-    <v-data-table :items="controls" :headers="headers" @click:row="rowClick" />
-  </v-card>
+  <v-skeleton-loader
+      class="mx-auto"
+      elevation="2"
+      type="heading, subtitle, list-item-two-line, divider, table-thead, table-tbody"
+      :loading=loading
+    >
+    <container>
+      <h1>{{framework.short_name}}</h1>
+      <h2>{{framework.full_name}}</h2>
+      <h3>{{framework.source}} - {{framework.geography}}</h3>
+      <a :href="framework.url">Source</a>
+      <v-card class="ma-4">
+        <v-data-table :items="controls" :headers="headers" @click:row="rowClick" />
+      </v-card>
+    </container>
+  </v-skeleton-loader>
 </template>
 
 <script>
@@ -38,6 +47,7 @@ export default {
       controls: [],
       framework: {},
       frameworkId: useRoute().params.id,
+      loading: true,
     };
   },
   created() {
@@ -46,6 +56,7 @@ export default {
       .then((data) => {
         this.framework = data;
         this.controls = data.controls;
+        this.loading = false;
       });
   },
   methods: {
