4f686c3938c301eda52cbc7adc1c77a3fac2a2be
Yosef Hoffman <yohoffman@gmail.com>
2024-10-30 23:54:30 -0400

    UI to add evidence

--- spa/src/pages/controls/[id].vue
+++ spa/src/pages/controls/[id].vue
@@ -16,7 +16,9 @@
       </v-row>
 
       <v-row>
-        <v-col>
+        <v-col
+          cols="12"
+          md="3">
           <v-card variant="tonal">
             <v-card-title v-if="!updatingStatus"><v-chip>{{status.status}}</v-chip></v-card-title>
             <v-card-subtitle v-if="!updatingStatus">Last updated: {{status.updated_at}}</v-card-subtitle>
@@ -38,50 +40,150 @@
             </v-card-actions>
           </v-card>
         </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col>
-      <v-sheet>
-        <p><b>Relative Control Weighting</b>: {{control.weight}}</p>
-        <p><b>NIST CSF Function Grouping</b>: {{control.nist_csf_function_grouping}}</p>
-        <p><b>PPTDF Applicability</b>: {{control.pptdf_applicability}}</p>
-      </v-sheet>
+        <v-spacer></v-spacer>
+        <v-col
+          cols="12"
+          md="8">
+          <v-sheet>
+            <p><b>Relative Control Weighting</b>: {{control.weight}}</p>
+            <p><b>NIST CSF Function Grouping</b>: {{control.nist_csf_function_grouping}}</p>
+            <p><b>PPTDF Applicability</b>: {{control.pptdf_applicability}}</p>
+          </v-sheet>
         </v-col>
       </v-row>
 
       <v-row>
         <v-col>
-      <v-sheet>
-        <h4>Methods To Comply With SCF Control</h4>
-        <p>{{control.methods}}</p>
-        <br>
-        <h4>SCF Control Question</h4>
-        <p v-if="control.questions">{{control.questions[0].question}}</p>
-      </v-sheet>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col>
-      <v-sheet :if="control.evidence_requests">
-        <h4>SCF Evidence Requests</h4>
-        <div v-for="evidence_request in control.evidence_requests">
-          <h5>{{evidence_request.identifier}} - {{evidence_request.area}}</h5>
-          <p><i>Artifact</i>: {{evidence_request.artifact}}</p>
-          <p>{{evidence_request.description}}</p>
+          <v-sheet>
+            <h3>SCF Control Question</h3>
+            <p v-if="control.questions">{{control.questions[0].question}}</p>
+          </v-sheet>
           <br>
-        </div>
-      </v-sheet>
+          <v-divider></v-divider>
         </v-col>
       </v-row>
 
       <v-row>
         <v-col>
-      <v-sheet :if="control.assessment_objectives">
-        <h4>SCF Assessment Objectives</h4>
-        <v-data-table :items="control.assessment_objectives" :headers="headers" />
-      </v-sheet>
+          <v-sheet v-if="control.evidence_requests">
+            <span class="text-h6 mr-5">SCF Evidence Requests</span>
+            <div v-for="evidence_request in control.evidence_requests">
+              <div class="text-subtitle-1 mr-5">
+                {{evidence_request.identifier}} - {{evidence_request.area}}
+                <v-btn
+                  class="text-none font-weight-regular"
+                  icon="mdi-receipt-text-plus-outline"
+                  size="x-small"
+                  variant="tonal"
+                  title="Add This Evidence"
+                  @click="addThisEvidence(evidence_request)"
+                ></v-btn>
+              </div>
+              <p><i>Artifact</i>: {{evidence_request.artifact}}</p>
+              <p>{{evidence_request.description}}</p>
+              <br>
+            </div>
+          </v-sheet>
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-sheet>
+            <span class="text-h5 mr-5">Evidence</span>
+            <v-dialog
+              v-model="evidenceDialog"
+              max-width="600"
+            >
+              <template v-slot:activator="{ props: activatorProps }">
+                <v-btn
+                  class="text-none font-weight-regular"
+                  icon="mdi-receipt-text-plus-outline"
+                  variant="tonal"
+                  v-bind="activatorProps"
+                  title="Add Evidence"
+                ></v-btn>
+              </template>
+
+              <v-card
+                prepend-icon="mdi-receipt-text-plus-outline"
+                title="Add Evidence"
+              >
+                <v-form @submit.prevent ref="evidenceForm" v-model="validEvidenceForm">
+                  <v-card-text>
+                    <v-row dense>
+                      <v-col
+                        cols="12"
+                        md="12"
+                      >
+                        <v-text-field
+                          v-model="evidenceTitle"
+                          hint="Short description of your evidence"
+                          label="Title"
+                          required
+                          :rules="[v => !!v || 'You must enter a title!']"
+                        ></v-text-field>
+                      </v-col>
+
+                      <v-col
+                        cols="12"
+                        md="12"
+                      >
+                        <v-textarea
+                          v-model="evidenceDescription"
+                          label="Details"
+                          auto-grow
+                          counter
+                          rows="5"
+                          required
+                          :rules="[v => !!v || 'You must enter details!']"
+                        ></v-textarea>
+                      </v-col>
+                    </v-row>
+
+                    <small class="text-caption text-medium-emphasis">*all fields required</small>
+                  </v-card-text>
+
+                  <v-divider></v-divider>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                      text="Close"
+                      variant="plain"
+                      @click="closeEvidenceForm()"
+                    ></v-btn>
+
+                    <v-btn
+                      color="primary"
+                      text="Save"
+                      variant="tonal"
+                      type="submit"
+                      @click="saveEvidence()"
+                    ></v-btn>
+                  </v-card-actions>
+                </v-form>
+              </v-card>
+            </v-dialog>
+            <v-data-table
+              :items="control.evidence"
+              :headers="evidenceHeaders"
+              no-data-text="No evidence found" />
+          </v-sheet>
+          <br>
+          <v-divider></v-divider>
+          <br>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-sheet v-if="control.assessment_objectives.length">
+            <span class="text-h5 mr-5">SCF Assessment Objectives</span>
+            <v-data-table :items="control.assessment_objectives" :headers="assessmentObjHeaders" />
+          </v-sheet>
         </v-col>
       </v-row>
     </v-container>
@@ -92,7 +194,22 @@
 export default {
   data() {
     return {
-      headers: [
+      evidenceHeaders: [
+        {
+          title: "Added",
+          key: "created_at",
+          width: "200",
+        },
+        {
+          title: "Title",
+          key: "title",
+        },
+        {
+          title: "Details",
+          key: "description",
+        },
+      ],
+      assessmentObjHeaders: [
         {
           title: "ID",
           key: "identifier",
@@ -122,6 +239,10 @@ export default {
       ],
       updatingStatus: false,
       newStatus: '',
+      evidenceDialog: false,
+      evidenceDescription: '',
+      evidenceTitle: '',
+      validEvidenceForm: true,
     };
   },
   created() {
@@ -137,7 +258,6 @@ export default {
   },
   methods: {
     saveStatus() {
-      console.log(this.newStatus);
       const requestOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -145,9 +265,40 @@ export default {
       };
       fetch(import.meta.env.VITE_AXIOS_BASE_URL + 'controls/' + this.controlId + '/status', requestOptions)
         .then(response => response.json())
-        .then(data => (this.status = data));
-      this.updatingStatus = false;
+        .then(data => {
+          this.status = data;
+          this.updatingStatus = false;
+        });
     },
+    saveEvidence() {
+      if (!this.validEvidenceForm) {
+        return;
+      }
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: this.evidenceTitle,
+          description: this.evidenceDescription,
+        })
+      };
+      fetch(import.meta.env.VITE_AXIOS_BASE_URL + 'controls/' + this.controlId + '/evidence', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          this.control.evidence.push(data);
+          this.closeEvidenceForm();
+        });
+    },
+    addThisEvidence(evidence_request) {
+      this.evidenceDescription = evidence_request.description + '\n----------\n';
+      this.evidenceTitle = evidence_request.identifier + ' - ' + evidence_request.artifact;
+      this.evidenceDialog = true;
+    },
+    closeEvidenceForm() {
+      this.evidenceDialog = false;
+      this.evidenceDescription = '';
+      this.evidenceTitle = '';
+    }
   },
 };
 </script>
