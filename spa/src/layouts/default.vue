30f86b8379f1f9b0c0dfd4f3b9b95b6f5ade8f65
Yosef Hoffman <yohoffman@gmail.com>
2024-11-07 08:23:36 -0500

    Add nav to top bar

--- spa/src/layouts/default.vue
+++ spa/src/layouts/default.vue
@@ -15,6 +15,17 @@
           contain
         ></v-img>
         <v-toolbar-title>Compliance Buddy</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn
+            flat
+            v-for="item in menuItems"
+            :key="item.title"
+            :to="item.path">
+            <v-icon left dark>{{ item.icon }}</v-icon>
+            {{ item.title }}
+          </v-btn>
+        </v-toolbar-items>
       </v-app-bar>
 
       <v-navigation-drawer
